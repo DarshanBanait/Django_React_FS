5a2365b8dee12b73170cccb151dcca883b7adfe1
Darshan Banait <darshanbanait21@outlook.com>
2024-07-27 16:09:39 +0530

    second commit - added Room model, view, serializer

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+
+class RoomView(generics.CreateAPIView): #generics.CreateAPIView helps to create a view like a form without me writing html and css code
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
