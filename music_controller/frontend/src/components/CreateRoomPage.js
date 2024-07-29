e8fc92908c37cbef66eb7f3aad4c9e4b7d2be81b
Darshan Banait <darshanbanait21@outlook.com>
2024-07-29 13:45:20 +0530

    half 6th

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { Button, Grid, Typography, TextField, FormHelperText, FormControl, Radio, RadioGroup, FormControlLabel } from "@mui/material";
+import { Link } from "react-router-dom";
+
 
 export default class CreateRoomPage extends Component {
+    defaultVotes = 2;
     constructor(props) {
         super(props);
     }
     render() {
-        return <h1>This is the create room page</h1>;
+        // 1 is 8 pixels
+        return <Grid container spacing={1}>
+            <Grid item xs={12} align = "center">
+                <Typography variant="h4">Create a new room</Typography>
+            </Grid>
+
+        </Grid>; 
     }
 }
